b126154d814834958de46bdcbca30421c4267958
ThestralWarrior <ashbroezio@outlook.com>
2024-10-21 23:39:47 +0530

    rename

--- src/main/java/com/application/textingapplication/TextingClientCLI.java
+++ src/main/java/com/application/textingapplication/TextingClientCLI.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class TextingClient {
+public class TextingClientCLI {
     public static void main(String[] args) {
         Socket soc = null;
         String ipAddress = "localhost";
